ed67858a1089b652627ca71c791890909f7e4fdb
unknown <mostafijur8741@gmail.com>
2022-10-29 20:04:20 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,10 @@
+let jashim=[12,14,18,20]
+console.log(jashim[10]);
+let moyna={age:15,number:555555,gender:'female'}
+console.log(moyna.name);
+function name(num1,num2) {
+   let total=(num1+num2);
+   return 
+}
+let result= name(18,20)
+console.log(result)
