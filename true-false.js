bd60468d52ada23d364ac8ca45eea0b023a3cca8
unknown <mostafijur8741@gmail.com>
2022-10-29 18:59:27 +0600

    first

---
+++ true-false.js
@@ -0,0 +1,23 @@
+// false
+// 0
+// ''
+// undefined
+//null
+//NaN
+// true
+//'0'
+//' '
+// {}
+// []
+
+
+let number=' '
+
+
+if (number) {
+    console.log('condition is true');
+    
+}
+else{
+    console.log('condition is false');
+}
