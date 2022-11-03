4c50a11d41d484b842dfd1faddb33ba77553fd44
unknown <mostafijur8741@gmail.com>
2022-11-03 19:08:35 +0600

    v2

---
+++ break-continue.js
@@ -0,0 +1,21 @@
+let num=[1,2,3,4,-5,-6,-7,8,9]
+for (let i = 0; i < num.length; i++) {
+   
+    if (num[i]>5) {
+        break;
+      
+        
+    }
+    // console.log(num[i])
+    
+}
+for (let i = 0; i < num.length; i++) {
+    const element = num[i];
+    if (num[i]<0) {
+        continue;
+       
+        
+    }
+    console.log(num[i]);
+    
+}
